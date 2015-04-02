5fe7f4d6c9dfa8ca4c1d83cadef112e7efdfb49a
Truong Thanh Ha <thienhabka@gmail.com>
2015-04-02 20:53:04 +0700

    Update ha.py

--- ha.py
+++ ha.py
@@ -1,53 +1,91 @@
 txt = open("/var/log/auth.log")
-cuoi = []
 
-cat = []
-a = txt.readlines()
+fail_ssh_last = []
+cut_fail_ssh_last = []
+
+fail_direct_login_last = []
+cut_fail_direct_login_last = []
+
+success_ssh_last = []
+cut_success_ssh_last = []
+
+success_direct_login_last = []
+cut_success_direct_login_last = []
+
+docfile = txt.readlines()
+
+tukhoa1 = ": Failed password"
+tukhoa11 = "invalid"
+
+tukhoa2 = ": FAILED LOGIN"
+tukhoa22 = "UNKNOWN"
+
+tukhoa3 = "pam_unix(login:session): session opened"
+
+tukhoa4 = ": Accepted password"
 
 
-for i in range(0,len(a)):
-    if ": Failed password"in a[i]:
-        solanfail = solanfail+1
-        cuoi.append(a[i])
-cat=cuoi[len(cuoi)-1].split(" ")
-if b in cat:
+for i in range(0,len(docfile)):
+    if tukhoa1 in docfile[i]:
+        fail_ssh_last.append(docfile[i])
+
+cut_fail_ssh_last=fail_ssh_last[len(fail_ssh_last)-1].split(" ")
+if tukhoa11 in cut_fail_ssh_last:
     print "{\n"
     print '\t"data":[\n\n'
     print "\t{\n"
-    
-    print '\t\t"{#LOGIN}":"FAIL",' +   '\t\t"{#USER}":\"Khong ton tai user '+str(cat[11])+"\","+'\t\t"{#IP}":\"'+str(cat[11])+" port "+str(cat[15])+"\""
-    print "\t}\n"
-    print "\n\t]\n"
-    print "}\n"
+    print '\t\t"{#LOGIN1}":"FAIL SSH",' +   '\t\t"{#USER}":\"Khong ton tai user '+str(cut_fail_ssh_last[11])+"\","+'\t\t"{#IP}":\"from '+str(cut_fail_ssh_last[13])+" port "+str(cut_fail_ssh_last[15])+"\""
+    print "\t},\n"
+
 else:
     print "{\n"
     print '\t"data":[\n\n'
     print "\t{\n"
-    print '\t\t"{#LOGIN}":"FAIL",' +  '\t\t"{#USER}":\"'+"User "+str(cat[9])+"\","+ '\t\t"{#IP}":\"'+str(cat[11])+" port "+str(cat[13])+"\""
-    print "\t}\n"
-    print "\n\t]\n"
-    print "}\n"
+    print '\t\t"{#LOGIN1}":"FAIL SSH",' +  '\t\t"{#USER}":\"'+"User "+str(cut_fail_ssh_last[9])+"\","+ '\t\t"{#IP}":\" from'+str(cut_fail_ssh_last[11])+" port "+str(cut_fail_ssh_last[13])+"\""
+    print "\t},\n"
 
 
-for i in range(0,len(a)):
-    if ": Accepted password"in a[i]:
-        solansuccess = solansuccess+1
-        cuoi2.append(a[i])
-cat=cuoi2[len(cuoi2)-1].split(" ")
-if b in cat:
-    print "{\n"
-    print '\t"data":[\n\n'
+
+
+
+for i in range(0,len(docfile)):
+    if tukhoa2 in docfile[i]:
+        fail_direct_login_last.append(docfile[i])
+
+cut_fail_direct_login_last=fail_direct_login_last[len(fail_direct_login_last)-1].split(" ")
+if tukhoa22 in cut_fail_direct_login_last:
     print "\t{\n"
     
-    print '\t\t"{#LOGIN}":"FAIL",' +   '\t\t"{#USER}":\"Khong ton tai user '+str(cat[11])+"\","+'\t\t"{#IP}":\"'+str(cat[11])+" port "+str(cat[15])+"\""
-    print "\t}\n"
-    print "\n\t]\n"
-    print "}\n"
+    print '\t\t"{#LOGIN2}":"FAIL DIRECT LOGIN",' +   '\t\t"{#USER}":\"Khong ton tai user '+"\","+'\t\t"{#IP}":\"'+" Authentication failure "+"\""
+    print "\t},\n"
+
 else:
-    print "{\n"
-    print '\t"data":[\n\n'
     print "\t{\n"
-    print '\t\t"{#LOGIN}":"FAIL",' +  '\t\t"{#USER}":\"'+"User "+str(cat[9])+"\","+ '\t\t"{#IP}":\"'+str(cat[11])+" port "+str(cat[13])+"\""
-    print "\t}\n"
-    print "\n\t]\n"
-    print "}\n"
+    print '\t\t"{#LOGIN2}":"FAIL DIRECT LOGIN",' +  '\t\t"{#USER}":\"'+"User "+str(cut_fail_direct_login_last[12])+"\","+ '\t\t"{#IP}":\"'+" Authentication failure "+"\""
+    print "\t},\n"
+
+
+
+for i in range(0,len(docfile)):
+    if tukhoa3 in docfile[i]:
+        success_direct_login_last.append(docfile[i])
+
+cut_success_direct_login_last = success_direct_login_last[len(success_direct_login_last)-1].split(" ")
+
+print "\t{\n"
+    
+print '\t\t"{#LOGIN3}":"SUCCESS DIRECT LOGIN",' +  '\t\t"{#USER}":\"'+"User "+str(cut_success_direct_login_last[11])+"\","+ '\t\t"{#IP}":\"'+" by "+str(cut_success_direct_login_last[13]).sit("\n")[0]+"\""
+print "\t},\n"
+
+
+
+for i in range(0,len(docfile)):
+    if tukhoa4 in docfile[i]:
+        success_ssh_last.append(docfile[i])
+cut_success_ssh_last=success_ssh_last[len(success_ssh_last)-1].split(" ")
+
+print "\t{\n"
+print '\t\t"{#LOGIN4}":"SUCCESS SSH",' +  '\t\t"{#USER}":\"'+"User "+str(cut_success_ssh_last[9])+"\","+ '\t\t"{#IP}":\"from '+str(cut_success_ssh_last[11])+" port "+str(cut_success_ssh_last[13])+"\""
+print "\t}\n"
+print "\n\t]\n"
+print "}\n"
